1b4d1bd0261e4fb25c1ca896a0ffb14ed2ca96a1
abdullahALshehrii <Abdullah.Alshehri.jed.k1@tuwaiq.edu.sa>
2021-11-01 23:50:23 +0300

    ....

--- homeworkSolution.kt
+++ homeworkSolution.kt
@@ -1,6 +1,22 @@
-# Kotlin Programming 
----
-## Learning Objectives ✨
-- defining class
-## Getting started 
-Open the [assignment.md](assignment.md) file to get started.
+fun main{
+    val nameOfStudent=Student()
+    nameOfstudent.name="|],]d"
+    nameOfStudent.age=24
+    nameOfStudent.GPA=4.68
+    println("the name:${nameOfStudent.name}")
+    nameOfStudent.speakAbilty()
+}
+class Student(){
+    var name:""
+    var age:Int=0
+    var GPA :Double=0.0
+
+    fun speakAbilty(){
+        println("student speak perfect")
+
+    }
+    fun adding(x:Int,Y:Int):Int{
+        return x +y
+    }
+
+}
